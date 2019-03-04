22652c606923fa2b683c767c1f5ce06fc657437c
guptamadhur <guptamadhur>
2019-03-04 16:24:31 +0530

    Java: Introduction

---
+++ Java/Welcome to Java.java
@@ -0,0 +1,11 @@
+# Author: Madhur Gupta
+# Github: github.com/guptamadhur
+# Project: Hacker Rank Practice Java
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
